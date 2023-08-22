230037ed435e8604d13e4cf4309bf3fc512b64f4
Aman Nijjar <amannijjar19@gmail.com>
2023-08-22 14:23:20 -0700

    Got Blue LED to turn on. TODO: Use a timer to make it blink

--- BlinkyLED/src/main.c
+++ BlinkyLED/src/main.c
@@ -1,12 +1,26 @@
 /* Main program. */
 #include "main.h"
 
-
-int main(void) {
+void SimpleIncrementExample(){
   uint32_t var1 = 2;
   int val = 0;
   while (1) {
     val += var1;
   }
+}
+
+int main(void) {
+  
+  // Works
+  // SimpleIncrementExample();
+
+  // Turn on the GPIOC Clock
+  SET_BIT(RCC->AHBENR, RCC_AHBENR_GPIOCEN);
+  // Set PC8 to output mode
+  SET_BIT(GPIOC->MODER, GPIO_MODER_MODER8_0);
+  // Set PC8 to High using ODR register;
+  SET_BIT(GPIOC->ODR, GPIO_ODR_8);
+
+  for(;;);
   return 0;
 }
