a4a537f6e852fc7601d4e06553978569863a5a4b
Aman Nijjar <amannijjar19@gmail.com>
2023-09-07 13:48:05 -0700

    Created a template project

---
+++ Empty Project/src/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "_rcc.h"
+#include "onboardPeriph.h"
+#include "timer.h"
+
+
+int main(void) {
+  // _SYSTEM_CORE_CLOCK_SETUP(); // Setup clock frequency in main.h
+
+
+  for(;;);
+  return 0;
+}
